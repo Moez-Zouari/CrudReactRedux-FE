d165d946a702c3b59e703212e873c3a165b2c77f
moezzouari <moez@octoplus.tech>
2023-08-28 14:17:36 +0100

    Ajout GetArticle dans ArticleSlice

---
+++ src/features/articleSlice.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { fetchArticles } from '../Services/ArticleService';
+
+export const getArticles = createAsyncThunk(
+    "article/getArticles",
+    async (_, thunkAPI) => {
+        const { rejectWithValue } = thunkAPI;
+        try {
+            const res = await fetchArticles();
+            return res.data;
+        }
+        catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const articleSlice = createSlice({
+    name: 'article',
+    initialState: {
+        articles: [],
+        article: {},
+        isLoading: false,
+        success: null,
+        error: null,
+    },
+
+
+    extraReducers: (builder) => {
+        //get articles
+        builder
+            .addCase(getArticles.pending, (state, action) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(getArticles.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.articles = action.payload;
+            })
+            .addCase(getArticles.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+                console.log("impossible de se connecter au serveur")
+            });
+
+    }
+})
+
+export default articleSlice.reducer;
