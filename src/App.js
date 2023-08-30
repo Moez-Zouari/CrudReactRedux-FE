c58dd543fe47b348874f55d71c03d0df89c23db7
moezzouari <moez@octoplus.tech>
2023-08-30 14:04:11 +0100

    Add ProductAppAdmin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Listarticles from './components/articleRedux/Listarticles';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
